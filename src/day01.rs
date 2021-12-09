f83fe6ba4f5fb443988433b0a9f386f18a23552b
Daniel Naschberger <daniel@naschi.at>
2021-12-09 19:28:12 +0100

    chore(day01): part2 of solutions

--- src/day01.rs
+++ src/day01.rs
@@ -2,7 +2,7 @@
 use std::fs;
 use std::path::Path;
 
-pub fn read_profile_file() -> Vec<u32> {
+fn read_profile_file() -> Vec<u32> {
     let filename = Path::new("./data/day01.txt");
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
@@ -11,11 +11,7 @@ pub fn read_profile_file() -> Vec<u32> {
     return depths
 }
 
-pub fn solution_day01() -> i32 {
-    let depths = read_profile_file();
-    return increase_values(&depths);
-}
-pub fn increase_values(depths: &[u32]) -> i32 {
+fn increase_values(depths: &[u32]) -> i32 {
     let mut increased_measurements = 0; 
     for (index, _depth) in depths.iter().enumerate() {
         if index == 0 {
@@ -28,26 +24,52 @@ pub fn increase_values(depths: &[u32]) -> i32 {
     return increased_measurements;
 }
 
+fn increase_values_moving_window(depths: &[u32]) -> i32 {
+    let mut depths_moving_window = Vec::new();
+    for (index, _depth) in depths.iter().enumerate() {
+        if index < 2 {
+            continue;
+        }
+        let sum = depths[index - 2] + depths[index - 1] + depths[index];
+        depths_moving_window.push(sum)
+    }
+    return increase_values(&depths_moving_window);
+}
+
+pub fn solution_day01() {
+    let depths = read_profile_file();
+    let result_day01_part1 = increase_values(&depths);
+    println!("The solution for the 1st part of the puzzle from day 01 is '{}'!", result_day01_part1);
+
+    let result_day01_part2 = increase_values_moving_window(&depths);
+    println!("The solution for the 2nd part of the puzzle from day 01 is '{}'!", result_day01_part2);
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_solution_day01_increase() {
+    fn test_increase_values_increase() {
         let result = increase_values(&[199, 200]);
         assert_eq!(1, result);
     }
 
     #[test]
-    fn test_solution_day01_decrease() {
+    fn test_increase_values_decrease() {
         let result = increase_values(&[200, 199]);
         assert_eq!(0, result);
     }
 
     #[test]
-    fn test_solution_day01_example() {
+    fn test_increase_values_example() {
         let result = increase_values(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263]);
         assert_eq!(7, result);
+    }
+
+    #[test]
+    fn test_increase_values_moving_window_example() {
+        let result = increase_values_moving_window(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263]);
+        assert_eq!(5, result);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 mod day01;
 
 fn main() {
-    day01::read_profile_file();
-    let result_day01 = day01::solution_day01();
-    println!("The solution for the puzzle of day 01 = '{}'!", result_day01)
+    day01::solution_day01();
 }
